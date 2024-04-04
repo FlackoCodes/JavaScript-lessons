cee1bd66d4d3689f9eef7c275b128c77b6ea6453
Flacko <lesliejoe2k2@gmail.com>
2024-04-04 05:35:07 +0000

    update script

--- Mordern Js/Js/script-9.js
+++ Mordern Js/Js/script-9.js
@@ -1,3 +1,22 @@
 // traversing the dom
-const parent = document.querySelector('.parent')
-console.log(parent.nodeName);
+const parent = document.querySelector('.parent');
+
+const children = document.querySelector('.children');
+console.log(children.parentNode);
+
+// sibling elements
+
+/*
+ nextSibling returns the next node (an element node, a text node or a comment node). 
+Whitespace between elements are also text nodes.
+
+*/
+
+const sibling1 = document.querySelector('.children');
+console.log(sibling1.nextSibling);
+
+const sibling2 = document.querySelector('.children')
+console.log(sibling2.nextElementSibling);
+
+const sibling3 = document.querySelector('.children')
+console.log(sibling3.previousSibling.textContent);
