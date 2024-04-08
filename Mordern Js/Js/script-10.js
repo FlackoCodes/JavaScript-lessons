97afa500484c0d3b71cafa4b8d5e1f403e8dd611
Flacko <lesliejoe2k2@gmail.com>
2024-04-08 20:51:52 +0000

    updated scripts

--- Mordern Js/Js/script-10.js
+++ Mordern Js/Js/script-10.js
@@ -14,4 +14,12 @@ clearBtn.addEventListener('click', addEvent);
 
 // the callback function is not called with parenthesis to prevent the function from firing automatically
 
-// event obkect (e)
+// event object (e)
+
+const obj = {
+    name:'Flacko',
+    age:26
+}
+
+const {age, name} = obj
+console.log(name);
