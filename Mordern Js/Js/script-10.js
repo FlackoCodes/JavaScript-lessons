b5148b90cdf0a0ac6cc31161f1b21c072f279478
Flacko <lesliejoe2k2@gmail.com>
2024-04-08 20:39:27 +0000

    updated scripts

--- Mordern Js/Js/script-10.js
+++ Mordern Js/Js/script-10.js
@@ -7,5 +7,11 @@
 const clearBtn = document.querySelector('#clear')
 
 const addEvent = () => {
-    console.log('button is clicked');
+    console.log('clear button is clicked');
 }
+
+clearBtn.addEventListener('click', addEvent);
+
+// the callback function is not called with parenthesis to prevent the function from firing automatically
+
+// event obkect (e)
