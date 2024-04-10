bf36d7d356d073a6ed1ef733edc840c405851e4d
Flacko <lesliejoe2k2@gmail.com>
2024-04-10 06:28:22 +0000

    updated files

--- Mordern Js/Js/script-11.js
+++ Mordern Js/Js/script-11.js
@@ -14,6 +14,16 @@ const onSelect = (e) =>{
     heading.textContent = e.target.value
 }
 
+const onChecked = (e) =>{
+    const isChecked = e.target.checked
+    if (isChecked) {
+        heading.textContent = `checkbox has been checked: ${isChecked}`
+    } else {
+        heading.textContent = `checkbox has npt been checked`
+    }
+}
+
 inputItem.addEventListener('input', onInput)
-priorityItem.addEventListener('change', onInput)
+priorityItem.addEventListener('change', onSelect)
+checkbox.addEventListener('input', onChecked)
 
